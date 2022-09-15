fe833a5e701adca45b0ea766dea216153ca037ef
leo <leoraker@gmail.com>
2022-09-15 14:13:31 -0400

    9-15 changes

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/App.jsx",
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+    ],
+    resolve: {
+        modules: [__dirname, 'node_modules'],
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node-modules/,
+                loader: require.resolve('babel-loader'),
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ['file-loader'],
+            },
+        ],
+    },
+};
